d1b691ccc2d4df51c5dc47ab0fc31faeb5a40c86
root <root@ip-172-31-34-226.ec2.internal>
2023-10-23 09:39:37 +0000

    add build folder

---
+++ build/precache-manifest.c48d1a138c870e8b783a46b1bc16645d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf740528679f8b60d6c9d4ca54d39da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "75bb4def04cebc5befe1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5ff3208074254b65c4ff",
+    "url": "/static/js/2.c71cdbdb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.c71cdbdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bb4def04cebc5befe1",
+    "url": "/static/js/main.fbcad405.chunk.js"
+  },
+  {
+    "revision": "205dba4f9959a084aa7c",
+    "url": "/static/js/runtime-main.c2a4c5c8.js"
+  }
+]);
